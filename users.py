4d79c25f8909a15275612767196025d90497d0b8
K.Kivi <72075784+kivik-beep@users.noreply.github.com>
2021-11-18 23:22:45 +0200

    code formatting fixes

--- users.py
+++ users.py
@@ -24,9 +24,9 @@ def logout():
 def register(username, password):
     hash_value = generate_password_hash(password)
     try:
-        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
         db.session.execute(sql, {"username":username, "password":hash_value})
         db.session.commit()
     except:
-       return False
-    return login(username, password) 
+        return False
+    return login(username, password)
