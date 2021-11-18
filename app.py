733d95ee602760d82783289136b2aef5913c269b
K.Kivi <72075784+kivik-beep@users.noreply.github.com>
2021-11-18 11:09:10 +0200

    Create raw site structure

--- app.py
+++ app.py
@@ -14,3 +14,20 @@ def index():
     result = db.session.execute("SELECT COUNT(*) FROM visitors")
     counter = result.fetchone()[0]
     return render_template("index.html", counter=counter) 
+
+@app.route("/register")
+def register():
+    return "Täällä voit luoda uuden käyttäjätunnuksen"
+
+@app.route("/login")
+def login():
+    return "Tällä sivulla voit kirjautua sisään"
+
+@app.route("/your-page")
+def welcome():
+    return "sisäänkirjautuminen palauttaa tänne, täällä samat vaihtoehdot kuin etusivulla + uusi resepti"
+    
+@app.route("/new")
+def new():
+    return "Täällä voit luoda uuden reseptin"
+    
