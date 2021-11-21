e6c956febfb5c0a8dc8290ae349f63fc8e3e36e3
K.Kivi <72075784+kivik-beep@users.noreply.github.com>
2021-11-21 23:16:34 +0200

    Add tables related to recipes

--- schema.sql
+++ schema.sql
@@ -1,2 +1,34 @@
-CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE recipe (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
+    instructions TEXT,
+    serves INTEGER,
+    active INTEGER,
+    passive INTEGER
+);
+
+CREATE TABLE incredients (
+    recipe_id REFERENCES recipe,
+    incredient_id REFERENCES incredient;
+    quantity DOUBLE,
+    scale TEXT
+);
+
+CREATE TABLE incredient (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
+    type TEXT
+);
+
+CREATE TABLE favorites {
+    user_id REFERENCES users,
+    recipe_id REFERENCES recipe
+);
+
+
